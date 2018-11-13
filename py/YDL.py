7cdc21d7cc2ab2a5434ba07c48606fe94be3b128
harryuan65 <harryuan.65@gmail.com>
2018-11-13 20:03:31 +0800

    Add files

---
+++ py/YDL.py
@@ -0,0 +1,55 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('[YOUTUBE_DL]Done downloading, now converting ...')
+
+
+ydl_opts_mp3 = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
+ydl_opts_mp4 = {
+    'format': 'bestvideo/best',
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
+#print('Type=?[mp4,mp3]',end = '')
+#tp=input()
+#print('Url=?',end = '')
+#url=input()
+
+def ydlt(tp):
+    if tp=='mp3':
+        with youtube_dl.YoutubeDL(ydl_opts_mp3) as ydl:
+            ydl.download([url])
+            print("[Console]Downloading as mp3...")
+    elif tp=='mp4':
+        with youtube_dl.YoutubeDL(ydl_opts_mp4) as ydl:
+            ydl.download([url])
+            print("[Console]Downloading as mp4...")
+    else:
+        return "[Console]Invalid argument"
+    
+    return "[Console]Done."
