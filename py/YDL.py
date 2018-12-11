923fb91829322e7bdf5e6d0ac5dcea4137f2d903
harryuan65 <harryuan.65@gmail.com>
2018-12-11 15:17:13 +0800

    Add ui to py info

--- py/YDL.py
+++ py/YDL.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import youtube_dl
-
+import threading
+stat=''
 
 class MyLogger(object):
     def debug(self, msg):
@@ -14,9 +15,9 @@ class MyLogger(object):
 
 
 def my_hook(d):
+    global stat
     if d['status'] == 'finished':
-        print('[YOUTUBE_DL]Done downloading, now converting ...')
-
+        stat='[Console]Done downloading, now converting ...'
 
 ydl_opts_mp3 = {
     'format': 'bestaudio/best',
@@ -35,12 +36,7 @@ ydl_opts_mp4 = {
     'progress_hooks': [my_hook],
 }
 
-#print('Type=?[mp4,mp3]',end = '')
-#tp=input()
-#print('Url=?',end = '')
-#url=input()
-
-def ydlt(tp):
+def downloadas(tp,url):
     if tp=='mp3':
         with youtube_dl.YoutubeDL(ydl_opts_mp3) as ydl:
             ydl.download([url])
@@ -53,3 +49,9 @@ def ydlt(tp):
         return "[Console]Invalid argument"
     
     return "[Console]Done."
+	
+def dl(a,b):
+  print('thread starts')
+  t = threading.Thread(target = downloadas,args = (a,b,))
+  t.start()
+  t.join()
--- py/main.py
+++ py/main.py
@@ -11,21 +11,31 @@ class AppWindow(QDialog):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.ui.pb_dl.clicked.connect(self.onClick_pbdl)
+        self.ui.pb_mp3.clicked.connect(self.onClick_pbmp3)
+        self.ui.pb_mp4.clicked.connect(self.onClick_pbmp4)
+        self.ui.lb_display.setWordWrap(True)
         self.urlstr=""
         self.mode=""
         self.show()
-		self.onClickStyle="background-color: rgb(0,0,35)"
+        self.onClickStyle="background-color: red;"
     def onClick_pbdl(self):
         try:
-            self.urlstr=self.ui.te_url.toPlainText()
-            self.ui.lb_display.setText(self.urlstr)
+            if self.mode!='':
+                self.urlstr=self.ui.te_url.toPlainText()
+                self.ui.lb_display.setText(self.ui.lb_display.text() + '\n' +self.urlstr)
+                str = ydl.dl(self.mode,self.urlstr)
+                self.ui.lb_display.setText(self.ui.lb_display.text() + '\n'+ydl.stat + '\n'+str)
+            else:
+                self.ui.lb_display.setText('[Error]No type specified.')
         except Exception as e:
             print(e)
+           
     def onClick_pbmp3(self):
         try:
             self.ui.pb_mp3.setStyleSheet(self.onClickStyle)
             self.ui.pb_mp4.setStyleSheet("")
             self.mode='mp3'
+            self.ui.lb_display.setText('As Mp3')
         except Exception as e:
             print(e)
     def onClick_pbmp4(self):
@@ -33,6 +43,8 @@ class AppWindow(QDialog):
             self.ui.pb_mp4.setStyleSheet(self.onClickStyle)
             self.ui.pb_mp3.setStyleSheet("")
             self.mode='mp4'
+            self.ui.lb_display.setText('As Mp4')
+#self.ui.lb_display.text()
         except Exception as e:
             print(e)
 app = QApplication(sys.argv)
